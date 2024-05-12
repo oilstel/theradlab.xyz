55dd6310a95e21534f405d2b5a08c307db309e7f
Elliott Cost <email@elliott.computer>
2024-05-12 16:04:10 +0200

    logo and blur on mobile

--- site/snippets/meta.php
+++ site/snippets/meta.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <?= css('assets/css/fonts.css?v=1.8') ?>
     <?= css('assets/css/global.css?v=1.8') ?>
     <?php
