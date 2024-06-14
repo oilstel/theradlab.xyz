bb90b6c9670ec6fce3fe9096379e1fff9bd2b4b2
Elliott Cost <email@elliott.computer>
2024-06-14 22:51:19 +0200

    page title

--- frontend/js/components/ProjectPage.js
+++ frontend/js/components/ProjectPage.js
@@ -1,13 +1,12 @@
-// assets/js/components/ProjectPage.js
 Vue.component('project-page', {
     data() {
-      return {
-        pageTitle: 'Rad Lab',
-        project: null,
-        projects: [],
-        currentIndex: 0,
-        loading: true
-      };
+        return {
+            pageTitle: 'Rad Lab',
+            project: null,
+            projects: [],
+            currentIndex: 0,
+            loading: true
+        };
     },
     template: `
       <div class="overlay">
@@ -24,64 +23,64 @@ Vue.component('project-page', {
       </div>
     `,
     created() {
-      this.fetchProjects();
+        this.fetchProjects();
     },
     methods: {
-      fetchProjects() {
-        fetch(`${window.apiUrl}projects`)
-          .then(response => response.json())
-          .then(data => {
-            this.projects = data.projects;
-            this.setCurrentProject();
-          })
-          .catch(error => console.error('Error fetching projects:', error));
-      },
-      setCurrentProject() {
-        const slug = this.$route.params.slug;
-        this.currentIndex = this.projects.findIndex(project => project.slug === slug);
-        if (this.currentIndex !== -1) {
-          this.fetchProject(slug);
-        } else {
-          this.project = null;
-          this.loading = false;
+        fetchProjects() {
+            fetch(`${window.apiUrl}projects`)
+                .then(response => response.json())
+                .then(data => {
+                    this.projects = data.projects;
+                    this.setCurrentProject();
+                })
+                .catch(error => console.error('Error fetching projects:', error));
+        },
+        setCurrentProject() {
+            const slug = this.$route.params.slug;
+            this.currentIndex = this.projects.findIndex(project => project.slug === slug);
+            if (this.currentIndex !== -1) {
+                this.fetchProject(slug);
+            } else {
+                this.project = null;
+                this.loading = false;
+            }
+        },
+        fetchProject(slug) {
+            fetch(`${window.apiUrl}projects/${slug}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.project = {
+                        title: data.title,
+                        layouts: data.layouts
+                    };
+                    document.title = `${this.project.title} – Rad Lab`;  // Set document title
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.error('Error fetching project content:', error);
+                    this.loading = false;
+                });
+        },
+        goToPrevious() {
+            if (this.currentIndex > 0) {
+                this.currentIndex--;
+                const previousSlug = this.projects[this.currentIndex].slug;
+                this.fetchProject(previousSlug);
+                this.$router.push({ name: 'project', params: { slug: previousSlug } });
+            }
+        },
+        goToNext() {
+            if (this.currentIndex < this.projects.length - 1) {
+                this.currentIndex++;
+                const nextSlug = this.projects[this.currentIndex].slug;
+                this.fetchProject(nextSlug);
+                this.$router.push({ name: 'project', params: { slug: nextSlug } });
+            }
         }
-      },
-      fetchProject(slug) {
-        fetch(`${window.apiUrl}projects/${slug}`)
-          .then(response => response.json())
-          .then(data => {
-            this.project = {
-              title: data.title,
-              layouts: data.layouts
-            };
-            this.loading = false;
-          })
-          .catch(error => {
-            console.error('Error fetching project content:', error);
-            this.loading = false;
-          });
-      },
-      goToPrevious() {
-        if (this.currentIndex > 0) {
-          this.currentIndex--;
-          const previousSlug = this.projects[this.currentIndex].slug;
-          this.fetchProject(previousSlug);
-          this.$router.push({ name: 'project', params: { slug: previousSlug } });
-        }
-      },
-      goToNext() {
-        if (this.currentIndex < this.projects.length - 1) {
-          this.currentIndex++;
-          const nextSlug = this.projects[this.currentIndex].slug;
-          this.fetchProject(nextSlug);
-          this.$router.push({ name: 'project', params: { slug: nextSlug } });
-        }
-      }
     },
     watch: {
-      '$route.params.slug'() {
-        this.setCurrentProject();
-      }
+        '$route.params.slug'() {
+            this.setCurrentProject();
+        }
     }
-  });
-  
+});
