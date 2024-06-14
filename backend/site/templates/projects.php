7f3861effb49e9b864de28f527b5f7c35886aec0
Elliott Cost <email@elliott.computer>
2024-06-14 22:32:32 +0200

    go

--- backend/site/templates/projects.php
+++ backend/site/templates/projects.php
@@ -10,13 +10,15 @@ $data = [
 $projects = page('projects')->children()->listed();
 
 foreach ($projects as $project) {
-    $data['projects'][] = [
-        'title' => $project->title()->value(),
-        'authors' => $project->authors()->value(),
-        'tags' => $project->tags()->value(),
-        'date' => $project->date()->value(),
-        'slug' => $project->slug()
-    ];
+    if (!$project->hide_from_index()->bool()) {  // Check if hide_from_index is false
+        $data['projects'][] = [
+            'title' => $project->title()->value(),
+            'authors' => $project->authors()->value(),
+            'tags' => $project->tags()->value(),
+            'date' => $project->date()->value(),
+            'slug' => $project->slug()
+        ];
+    }
 }
 
 echo json_encode($data);
